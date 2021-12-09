283a47a2d72cc98c297d5fcde7b906a887d3e8ff
Luiz Silva <luizfrdasilva@gmail.com>
2021-12-09 17:46:41 -0300

    Adjust requirements and setup.py

--- setup.py
+++ setup.py
@@ -1,31 +1,30 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 from scrapy_sticky_meta_params import __version__
 
-requirements = ["scrapy>=2.0"]
+with open("README.md") as f:
+    readme = f.read()
 
 setup(
     name="scrapy-sticky-meta-params",
     version=__version__,
+    license="MIT license",
     description="A spider middleware that forwards meta params through subsequent requests.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
     author="Luiz Francisco Rodrigues da Silva",
     author_email="luizfrdasilva@gmail.com",
     url="https://github.com/heylouiz/scrapy-sticky-meta-params",
-    packages=find_packages(include=["scrapy_sticky_meta_params"]),
-    include_package_data=True,
-    install_requires=requirements,
-    license="MIT license",
-    zip_safe=False,
+    packages=["scrapy_sticky_meta_params"],
+    platforms=["Any"],
     keywords="scrapy meta middleware",
+    include_package_data=True,
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.9",
     ],
+    install_requires=["scrapy>=1.6.0"],
 )
